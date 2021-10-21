4f2fe6b06b5c9013eee22bf6d401937c7b6f26df
Raluca Suciu <ralucasuciuart@gmail.com>
2021-10-22 01:18:52 +0300

    Added document ready event

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
-// go-to-top button slide-up animation on scroll
-$(window).scroll(function () {
-    if ($(this).scrollTop() != 0) {
-        $('#top_btn').slideDown(".3s", "swing");
-    } else {
-        $('#top_btn').slideUp(".3s", "swing");
-    }
+$(document).ready(function () {
+
+    // go-to-top button slide-up animation on scroll
+    $(window).scroll(function () {
+        if ($(this).scrollTop() != 0) {
+            $('#top_btn').slideDown(".3s", "swing");
+        } else {
+            $('#top_btn').slideUp(".3s", "swing");
+        }
+    });
+
 });
